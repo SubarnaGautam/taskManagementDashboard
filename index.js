a2ff748f40a7176c51940855e1a35c05edb2ea06
suborn <whatwevedecided@gmail.com>
2024-01-18 00:19:57 +1100

    new function created

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const admin = document.getElementById('admin');
-const teammember = document.getElementById('teammember');
+const teammember = document.getElementById('team');
 const result = document.getElementById('result');
 
-function openTeamPage() {
-    if (admin.checked || teammember.checked) {
-        window.open('./resources/team.html', '_blank');
-    } else {
-        result.textContent = `Please select user type`;
+function selectOnlyThis(id) {
+    var checkboxes = document.getElementsByName("teamm");
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.id !== id) {
+            checkbox.checked = false;
+        }
+    });
+}
+function openTeamPage(){
+    if(team.checked){
+        window.location.href ="team.html";
+    }else{
+        document.getElementById("result").innerText = "Please select Team Member.";
+
     }
+
 }
