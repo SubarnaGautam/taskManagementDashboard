5472415d87f3d24a60d2184691b7d155347b0e1a
suborn <whatwevedecided@gmail.com>
2024-01-20 17:03:41 +1100

    more styling applied

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const admin = document.getElementById('admin');
 const team = document.getElementById('team');
 const result = document.getElementById('result');
-
+const emailInput = document.getElementById('ademail');
+const passwordInput = document.getElementById('adpassword');
+const teamEmailInput = document.getElementById('tmemail');
+const teamPasswordInput = document.getElementById('tmpassword');
+const loginadmin = document.getElementById('login');
+const loginResult = document.getElementById('loginResult');
+const teamLoginResult = document.getElementById('teamLoginResult');
+const backTag = document.getElementById('back');
 function selectOnlyThis(id) {
     var checkboxes = document.getElementsByName("team");
     checkboxes.forEach(function (checkbox) {
@@ -10,9 +17,9 @@ function selectOnlyThis(id) {
         }
     });
 }
-function openTeamPage(){
+function openTeamPage(button){
     if(team.checked){
-        window.location.href ="team.html";
+        window.location.href ="teamlogin.html";
     } else if(admin.checked){
         window.location.href ="admin.html";
 
@@ -24,4 +31,26 @@ function openTeamPage(){
     }
 
 }
+function adminLogin(){
+    if (emailInput.value === 'admin@gmail.com' && passwordInput.value === '1234'){
+        window.location.href = "welcomeadmin.html";
+    }
+    else{
+        loginResult.innerText = "Incorrect Either email or password";
+    }
+}
+
+function teamLogin(){
+    if (teamEmailInput.value === 'team@gmail.com' && teamPasswordInput.value === '1234'){
+        window.location.href = "team.html";
+    }
+    else{
+        teamLoginResult.innerText = "Incorrect Either email or password";
+    }
+}
+function clickBack(){
+    window.location.href ="teamlogin.css";
+}
+
+
 
